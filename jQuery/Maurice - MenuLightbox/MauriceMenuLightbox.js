47f21cea556508e026f91151e4e5c3a39f2e6b86
mauricesandoval <m239111@gmail.com>
2016-06-24 12:56:46 -0700

    Adding Files

---
+++ jQuery/Maurice - MenuLightbox/MauriceMenuLightbox.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $("li").hide();
+    $(":button").mouseenter(function () {
+        $("li").slideToggle(1000);
+    });
+    $(":button").mouseclick(function () {
+        $("li").slideToggle(1000);
+    });
+
+});
